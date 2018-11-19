1095024c5418d290f4db494d6371aac1603b7f8b
linlanye <linlanye@sina.cn>
2018-11-19 20:37:53 +0800

    测试安装

--- src/installer/Installer.php
+++ src/installer/Installer.php
@@ -3,7 +3,7 @@
  * @Author:             林澜叶(linlanye)
  * @Contact:            <linlanye@sina.cn>
  * @Date:               2018-11-19 15:52:58
- * @Modified time:      2018-11-19 20:26:53
+ * @Modified time:      2018-11-19 20:37:47
  * @Description:        composer安装器
  */
 namespace basement\installer;
@@ -42,6 +42,7 @@ class BasementInstaller extends LibraryInstaller
     }
     public function supports($packageType)
     {
+        var_dump($packageType);
         return 'basement-basement' === $packageType;
     }
 }
