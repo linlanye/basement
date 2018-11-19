e4a4e11eb608f337f09be36d737a2b1f658ad0ae
linlanye <linlanye@sina.cn>
2018-11-19 14:44:03 +0800

    增加Config合并配置方法

--- src/Config.php
+++ src/Config.php
@@ -3,7 +3,7 @@
  * @Author:             林澜叶(linlanye)
  * @Contact:            <linlanye@sina.cn>
  * @Date:               2017-06-20 11:53:48
- * @Modified time:      2018-07-11 15:36:53
+ * @Modified time:      2018-11-19 14:43:35
  * @Description:        配置类规范，建议实现本类时不调用任何basement类，防止出现间接递归
  */
 namespace basement;
@@ -22,6 +22,7 @@ trait Config
      * 改变置配置内容
      * @param string $name     配置名
      * @param array  $content  配置内容
+     * @param bool   是否设置成功
      */
     public static function set(string $name, array $content) : bool
     {}
@@ -32,5 +33,15 @@ trait Config
      * @return bool         是否已加载
      */
     public static function exists(string $name): bool
+    {}
+
+    /**
+     * 向已有的配置合并内容，若配置内容不存在，则等效于set方法
+     * @param  string $name        配置名
+     * @param  array  $content     合并内容
+     * @param  bool   $isRecursive 是否递归合并，默认否
+     * @return bool   是否合并成功
+     */
+    public static function merge(string $name, array $content, bool $isRecursive = false): bool
     {}
 }
